c94069e760cf2c6bd9d9615576814da0e7c79d0a
GianniBuoni <162598175+GianniBuoni@users.noreply.github.com>
2025-02-10 19:31:36 -0500

    posts table and queries

---
+++ sql/queries/posts.sql
@@ -0,0 +1,21 @@
+-- name: CreatePost :one
+INSERT INTO posts (
+  id,
+  created_at,
+  updated_at,
+  title,
+  description,
+  published_at,
+  feed_id
+) VALUES ( $1, $2, $3, $4, $5, $6, $7 )
+RETURNING *;
+
+-- name: GetPostsForUser :many
+SELECT posts.*
+  FROM posts
+  INNER JOIN feed_follows ff
+  ON posts.feed_id = ff.feed_id
+  INNER JOIN users ON ff.user_id = users.id
+  WHERE users.id = $1
+  ORDER BY posts.published_at DESC
+  LIMIT $2;
